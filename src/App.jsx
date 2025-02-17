2fdc7d0ca384cf000834c2e00bc10e9ce24eab2e
ChristmasLover12345 <madnes0205@gmail.com>
2025-02-17 14:31:17 -0800

    started on navbar, currently stuck in a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+  
       
-    </>
+    <div className='w-full h-screen flex flex-col items-center'>
+
+      <div className="Bg-[#000324] h-[120px] w-full flex justify-center items-end">
+
+        <p className='mx-3'>Overview</p>
+        <p className='mx-3'>Rooms</p>
+        <p className='mx-3'>Packages</p>
+        <img src="./assets/Logo.png" alt="Logo" />
+        <p className='mx-3'>Amenities</p>
+        <p className='mx-3'>Contact</p>
+        <p className='mx-3'>Reserve</p>
+
+      </div>
+
+    </div>
+ 
   )
 }
 
