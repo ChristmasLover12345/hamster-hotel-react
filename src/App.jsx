219454d9a408bafa543b0b5be5334230c0f6672d
ChristmasLover12345 <madnes0205@gmail.com>
2025-02-17 14:53:21 -0800

    saving

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,15 @@ function App() {
       
     <div className='w-full h-screen flex flex-col items-center'>
 
-      <div className="Bg-[#000324] h-[120px] w-full flex justify-center items-end">
+      <div className="bg-[#000324] h-[120px] w-full flex justify-center items-end">
 
-        <p className='mx-3'>Overview</p>
-        <p className='mx-3'>Rooms</p>
-        <p className='mx-3'>Packages</p>
-        <img src="./assets/Logo.png" alt="Logo" />
-        <p className='mx-3'>Amenities</p>
-        <p className='mx-3'>Contact</p>
-        <p className='mx-3'>Reserve</p>
+        <p className='mx-3 mb-2'>Overview</p>
+        <p className='mx-3 mb-2'>Rooms</p>
+        <p className='mx-3 mb-2'>Packages</p>
+        <img src="./assets/Logo.png" alt="Logo" className='mx-3 mb-2' />
+        <p className='mx-3 mb-2'>Amenities</p>
+        <p className='mx-3 mb-2'>Contact</p>
+        <p className='mx-3 mb-2'>Reserve</p>
 
       </div>
 
